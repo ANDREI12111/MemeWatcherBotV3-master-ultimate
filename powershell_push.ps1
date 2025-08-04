81fc7f86a24818ca551fdba8f69c59d179ab6493
ANDREI12111 <youpiter222@gmail.com>
2025-08-04 18:36:56 +0200

    рџљЂ Initial commit: MemeWatcherBotV3-master-ultimate

---
+++ powershell_push.ps1
@@ -0,0 +1,7 @@
+cd "$PSScriptRoot"
+git init
+git remote add origin https://github.com/ANDREI12111/MemeWatcherBotV3-master-ultimate.git
+git add .
+git commit -m "🚀 Initial commit: MemeWatcherBotV3-master-ultimate"
+git branch -M main
+git push -u origin main
